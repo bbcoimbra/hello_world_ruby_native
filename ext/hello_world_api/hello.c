2b6bd85e0a3547cd3a752c6f8a60f42b6088c235
Bruno Coimbra <bbcoimbra@gmail.com>
2010-09-18 11:51:43 -0300

    added HelloWorld#hello_world

--- ext/hello_world_api/hello.c
+++ ext/hello_world_api/hello.c
@@ -7,8 +7,13 @@ VALUE greeting(VALUE self, VALUE str){
 	return rb_str_new2(strcat(str_hello, str_ptr));
 }
 
+VALUE hello_world(VALUE self){
+	return rb_str_new2("Hello World!\n");
+}
+
 VALUE cHello;
 void Init_hello_world_api(){
 	cHello = rb_define_class("HelloWorld", rb_cObject);
 	rb_define_method(cHello, "greeting", greeting, 1);
+	rb_define_method(cHello, "hello_world", hello_world, 0);
 }
